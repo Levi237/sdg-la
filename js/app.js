e9f4b7b993c3cb9aac38b6045eeaaf1a35e88bbc
Levi Winans <levieiko@gmail.com>
2019-09-05 11:29:30 -0700

    toggle on desktop and phone

--- js/app.js
+++ js/app.js
@@ -4,16 +4,22 @@
 const onFitGuide = document.getElementById('fit-guide')
 onFitGuide.addEventListener('click', (e) => {
     toggle(e);
+    onCare.classList.remove('active');
+    onMaterials.classList.remove('active');
 })
 
 const onCare = document.getElementById('care')
 onCare.addEventListener('click', (e) => {
     toggle(e);
+    onMaterials.classList.remove('active');
+    onFitGuide.classList.remove('active');
 })
 
 const onMaterials = document.getElementById('materials')
 onMaterials.addEventListener('click', (e) => {
     toggle(e);
+    onCare.classList.remove('active');
+    onFitGuide.classList.remove('active');
 })
 
 toggle = e => {
