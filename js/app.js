6dd0e39d85bae60e6c66d54631ef31bc0f2b9017
Levi Winans <levieiko@gmail.com>
2019-09-05 15:55:23 -0700

    alternate between + and -

--- js/app.js
+++ js/app.js
@@ -1,25 +1,44 @@
 //********************************************************************************//
 //*****************************   ACCORDIAN TOGGLE   *****************************//
 //********************************************************************************//
-const onFitGuide = document.getElementById('fit-guide')
+const onFitGuide    = document.getElementById('fit-guide')
+const onCare        = document.getElementById('care')
+const onMaterials   = document.getElementById('materials')
+// const plustFitGuide = document.getElementsByClassName('plus-fit-guide')
+// const plustCare     = document.getElementsByClassName('plus-care')
+// const plusMaterials = document.getElementsByClassName('plus-materials')
+
 onFitGuide.addEventListener('click', (e) => {
     toggle(e);
     onCare.classList.remove('active');
     onMaterials.classList.remove('active');
+    // plustFitGuide.classList.add('test');
+    // plustCare.classList.remove('test');
+    // plusMaterials.classList.remove('test');
+    // plustFitGuide.setAttribute("display", "none")
+    // plustCare.setAttribute("display", "block")
+    // plusMaterials.setAttribute("display", "block")
 })
 
-const onCare = document.getElementById('care')
 onCare.addEventListener('click', (e) => {
     toggle(e);
     onMaterials.classList.remove('active');
     onFitGuide.classList.remove('active');
+    // plustFitGuide.classList.remove('test');
+    // plustFitGuide.setAttribute("display", "block")
+    // plustCare.setAttribute("display", "block")
+    // plusMaterials.setAttribute("display", "block")
 })
-
-const onMaterials = document.getElementById('materials')
 onMaterials.addEventListener('click', (e) => {
     toggle(e);
     onCare.classList.remove('active');
     onFitGuide.classList.remove('active');
+    // plustFitGuide.classList.remove('test');
+    // plustCare.classList.remove('test');
+    // plusMaterials.classList.add('test');
+    // plustFitGuide.setAttribute("display", "block")
+    // plustCare.setAttribute("display", "block")
+    // plusMaterials.setAttribute("display", "block")
 })
 
 toggle = e => {
