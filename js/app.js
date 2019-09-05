7dd3791a0298092064756e6d85742c55cc677e5e
Levi Winans <levieiko@gmail.com>
2019-09-05 10:57:52 -0700

    clean up toggle code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
-// add event listender
-
+//********************************************************************************//
+//*****************************   ACCORDIAN TOGGLE   *****************************//
+//********************************************************************************//
 const onFitGuide = document.getElementById('fit-guide')
-// const onClick = document.querySelector('.details')
 onFitGuide.addEventListener('click', (e) => {
     toggle(e);
 })
 
 const onCare = document.getElementById('care')
-// const onClick = document.querySelector('.details')
 onCare.addEventListener('click', (e) => {
     toggle(e);
 })
 
 const onMaterials = document.getElementById('materials')
-// const onClick = document.querySelector('.details')
 onMaterials.addEventListener('click', (e) => {
     toggle(e);
 })
 
 toggle = e => {
-    console.log("clicked")
     e.currentTarget.classList.toggle('active');
 }
