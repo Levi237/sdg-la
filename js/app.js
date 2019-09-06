88f1f8908ce59a114996aa0330b5343fbe1951b4
Levi Winans <levieiko@gmail.com>
2019-09-05 18:20:49 -0700

    can propogate hard coded array, need to map through data.details perhaps.

--- js/app.js
+++ js/app.js
@@ -11,11 +11,16 @@ const data = [{
   }, {
     id: "materials",
     title: "MATERIALS",
-    details: {
-        cashmere: 50,
-        wool: 46,
-        modal: 4
-    },
+    details: [{
+        name: "cashmere",
+        percentage: 50
+    }, {
+        name: "wool",
+        percentage: 46
+    }, {
+        name: "modal",
+        percentage: 4
+    }],
     paragraph: "Sit amet mattis vulputate enim nulla aliquet. Non pulvinar neque laoreet suspendisse interdum consectetur. Tellus in metus vulputate eu. Consectetur lorem donec massa sapien. Tellus id interdum velit laoreet id. In dictum non consectetur a. Lacus suspendisse faucibus interdum posuere lorem ipsum. Gravida in fermentum et sollicitudin ac orci phasellus egestas. Senectus et netus et malesuada fames ac turpis egestas integer. Aliquam vestibulum morbi blandit cursus risus at ultrices mi. Volutpat consequat mauris nunc congue. Ac tortor vitae purus faucibus ornare suspendisse sed. Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. Suspendisse interdum consectetur libero id faucibus nisl. Sit amet venenatis urna cursus. Vestibulum lectus mauris ultrices eros in cursus turpis. Porttitor lacus luctus accumsan tortor posuere ac ut consequat. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida."
   }]
 
@@ -27,25 +32,19 @@ const thisContainer = document.getElementById('container')
 //********************************************************************************//
 
 // GOAL IS TO LOOP THROUGH DATA TO PRODUCE FITNESS LIST
-// if ( data ) {
-    const launchData = () => {
-    
 
+const launchData = () => {
+    data.map(data => {
+        if (!data.details) {
+            thisContainer.insertAdjacentHTML('afterbegin', `<div id="${data.id}" class="title"><section>${data.title}</section><span class="plus">+</span><span class="minus">-</span><div class="details"><div class="detail-section"></div><p>${data.paragraph}</p></div></div>`);
+        } else {
+            thisContainer.insertAdjacentHTML('afterbegin', `<div id="${data.id}" class="title"><section>${data.title}</section><span id="" class="plus">+</span><span id="" class="minus">-</span><div class="details"><div class="detail-section"><section>${data.details[0].percentage}%<div>${data.details[0].name}</div></section><section>${data.details[1].percentage}%<div>${data.details[1].name}</div></section><section>${data.details[2].percentage}%<div>${data.details[2].name}</div></section></div><hr style="width: 40px; margin: 0 auto 3px auto"><p>${data.paragraph}</p></div></div>
+            `);
+        }
+    });
+}
+launchData();
 
-
-        console.log(data, "second data call with if statement")
-        data.map(data => {
-            // return(
-                // <div>YAY</div>
-                console.log(data, "data from map")
-                thisContainer.insertAdjacentHTML('afterbegin', `<div id="${data.id}" class="title"><section>${data.title}</section><span class="plus">+</span><span class="minus">-</span><div class="details"><div class="detail-section"></div><p>${data.paragraph}</p> </div></div>`)
-            // )
-        })
-    }
-    
-    
-    launchData();
-    // }
 //********************************************************************************//
 //*****************************   ACCORDIAN TOGGLE   *****************************//
 //********************************************************************************//
