67d72e79e418a5a2cb94527381428d4482681b13
Levi Winans <levieiko@gmail.com>
2019-09-05 17:32:08 -0700

    add nested details values to materials in data array

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ const data = [{
     paragraph: "Auctor elit sed vulputate mi sit amet mauris. Ultricies integer quis auctor elit sed. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Tincidunt dui ut ornare lectus. Vitae aliquet nec ullamcorper sit amet risus nullam eget. Sed risus pretium quam vulputate dignissim suspendisse in est. In egestas erat imperdiet sed euismod nisi porta. Eget duis at tellus at urna condimentum mattis pellentesque. Adipiscing elit ut aliquam purus. Nulla pharetra diam sit amet nisl suscipit. Magna eget est lorem ipsum dolor sit amet consectetur. Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Tortor aliquam nulla facilisi cras fermentum odio eu. Gravida cum sociis natoque penatibus et magnis dis parturient. Tortor consequat id porta nibh venenatis. Blandit massa enim nec dui nunc mattis. Penatibus et magnis dis parturient montes nascetur ridiculus."
   }, {
     title: "MATERIALS",
+    details: {
+        cashmere: 50,
+        wool: 46,
+        modal: 4
+    },
     paragraph: "Sit amet mattis vulputate enim nulla aliquet. Non pulvinar neque laoreet suspendisse interdum consectetur. Tellus in metus vulputate eu. Consectetur lorem donec massa sapien. Tellus id interdum velit laoreet id. In dictum non consectetur a. Lacus suspendisse faucibus interdum posuere lorem ipsum. Gravida in fermentum et sollicitudin ac orci phasellus egestas. Senectus et netus et malesuada fames ac turpis egestas integer. Aliquam vestibulum morbi blandit cursus risus at ultrices mi. Volutpat consequat mauris nunc congue. Ac tortor vitae purus faucibus ornare suspendisse sed. Egestas maecenas pharetra convallis posuere morbi leo urna molestie at. Suspendisse interdum consectetur libero id faucibus nisl. Sit amet venenatis urna cursus. Vestibulum lectus mauris ultrices eros in cursus turpis. Porttitor lacus luctus accumsan tortor posuere ac ut consequat. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida."
   }]
 
